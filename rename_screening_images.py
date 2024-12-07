c3b44f9372984029e6ff01f81323b720c42c04d6
Xin <michaelniki1988@gmail.com>
2024-12-06 19:16:31 -0800

    [feat] add parallel job and women code

--- rename_screening_images.py
+++ rename_screening_images.py
@@ -21,18 +21,26 @@ examples = [
 
 """
 
+# TODO: use parallel jobs to copy files.
+# TODO: create IO files to avoid duplicates.
+
 import os
 import re
 import glob
 import csv
 import shutil
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import Optional
 
-INPUT_PATH = "/Users/XinNiuAdmin/Library/CloudStorage/Box-Box/finished_gallery"
-OUTPUT_PATH = "/Users/XinNiuAdmin/Library/CloudStorage/Box-Box/finished_gallery_renamed"
-LOG_FILE = "/Users/XinNiuAdmin/Library/CloudStorage/Box-Box/finished_gallery_renamed/rename_log.csv"
+from mne.io.curry.curry import FILE_EXTENSIONS
+
+INPUT_PATH = "/Users/XinNiuAdmin/Library/CloudStorage/Box-Box/finished_gallery_renamed_women_coded"
+OUTPUT_PATH = "/Users/XinNiuAdmin/Library/CloudStorage/Box-Box/ScreeningStimuli/images"
+LOG_FILE = "/Users/XinNiuAdmin/Library/CloudStorage/Box-Box/ScreeningStimuli/images_rename_log.csv"
 
 CLASS_CODE_INDEX = [0, 2, 15, 6, 27]  # [People Animals Buildings Men Plants]
 DELETE_EXISTING_FILES = True
+FILE_EXTENSION = '.jpg'
 
 
 def make_name(img_name:str, index:int, class_code:str, suffix:str) -> str:
@@ -47,20 +55,48 @@ def make_name(img_name:str, index:int, class_code:str, suffix:str) -> str:
     img_name = re.sub(r'_te?xt.*', '', img_name)
     img_name = img_name.replace('_', '-')
 
-    class_code = ''.join(class_code[i] for i in CLASS_CODE_INDEX)
-    new_name = f"{img_name}{text}_id{index:06d}_{class_code}.jpg"
+    # class_code = ''.join(class_code[i] for i in CLASS_CODE_INDEX)
+    # add additional code 0 in the 4th position:
+    if len(class_code) < 6:
+        print("modify class code.")
+        class_code = class_code[:4] + '0' + class_code[4:]
+    else:
+        print("use current class code.")
+
+    new_name = f"{img_name}{text}_id{index:06d}_{class_code}{FILE_EXTENSION}"
     return new_name
+
+
+def rename_image(img_path:str, image_id:int) -> Optional[str]:
+    img = os.path.basename(img_path)
+    # Update regex based on your requirements
+    match = re.search(r'(^[\w.\'&()\[\]\u0300-\u036F-]*)_(i?d\d{6,24})_(\d{5,6})(.*)?.jpg', img)
+    if match:
+        img_name, id_code, class_code, suffix = match.groups()
+        new_name = make_name(img_name, image_id, class_code, suffix)
+        dest_file = os.path.join(OUTPUT_PATH, new_name)
+        if os.path.exists(dest_file):
+            print(f"skip exist: {new_name}")
+        else:
+            shutil.copy(img_path, dest_file)
+            print(f"rename file: {img} to {new_name}")
+            return new_name
+    else:
+        print(f"not matched: {img}")
+
+    return None
 
 if __name__ == "__main__":
 
     if not os.path.exists(OUTPUT_PATH):
         os.makedirs(OUTPUT_PATH, exist_ok=True)
 
-    image_files = glob.glob(os.path.join(INPUT_PATH, '*.jpg'))
+    image_files = glob.glob(os.path.join(INPUT_PATH, f'*{FILE_EXTENSION}'))
     renamed_files = glob.glob(os.path.join(OUTPUT_PATH, '*'))
     if DELETE_EXISTING_FILES:
         for file in renamed_files:
             if os.path.isfile(file):
+                print(f"remove existing file: {file}")
                 os.remove(file)
 
         start_index = 0
@@ -68,20 +104,15 @@ if __name__ == "__main__":
         start_index = len(renamed_files)
 
     renamed_files = []
-    for img_path in image_files:
-        img = os.path.basename(img_path)
-        start_index += 1
-
-        match = re.search(r'(^[\w.\'&()\[\]\u0300-\u036F-]*)_(i?d\d{6,24})_(\d{28})(.*)?.jpg', img)
-        if match:
-            img_name, id_code, class_code, suffix = match.groups()
-            new_name = make_name(img_name, start_index, class_code, suffix)
-            renamed_files.append((img, new_name))
-            shutil.copy(img_path, os.path.join(OUTPUT_PATH, new_name))
-            print(f"rename file: {img} to {new_name}")
-            continue
-
-        print(f"not matched: {img}")
+    with ThreadPoolExecutor() as executor:
+        futures = {
+            executor.submit(rename_image, img_path, start_index + idx): img_path
+            for idx, img_path in enumerate(image_files)
+        }
+        for future in as_completed(futures):
+            result = future.result()
+            if result:
+                renamed_files.append(result)
 
     with open(LOG_FILE, 'w', newline='') as file:
         writer = csv.writer(file)
